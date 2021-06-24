ba763a2e516219f7a0eb55c89a1e04d175600f5e
Bropocalypse Team <amineninja9@gmail.com>
2021-06-24 19:19:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,56 @@
 from tkinter import * 
 from tkinter.ttk import *
 from tkinter import messagebox
-from encrypt import main as enc
-from decrypt import main as dec
+from enc import main as enc
+from dec import main as dec
 
-master = Tk()
-master.resizable(FALSE, FALSE)
-master.title("Home")
-messagebox.showinfo("Notice !", ''.join('All Rights Reserved To Bropocalypse Team'))
-  
-master.geometry("220x150")
-  
-label = Label(master, 
-              text ="V 1.0")
-  
-label.pack(pady = 10)
+def main():
+    master = Tk()
+    master.resizable(FALSE, FALSE)
+    master.title("Home")
+    messagebox.showinfo("Notice !", ''.join('All Rights Reserved To Bropocalypse Team'))
+    
+    master.geometry("220x200")
+    
+    label = Label(master, 
+                text ="V 2.0")
+    
+    label.pack(pady = 10)
 
-btn = Button(master, 
-             text ="Encrypt", 
-             command = enc)
-btn2 = Button(master, 
-             text ="Decrypt", 
-             command = dec)
+    btn = Button(master, 
+                text ="Generate Public/Private Key", 
+                command = gen)
 
-btn.pack(pady = 7)
-btn2.pack(pady = 7)
-Quit = Button(master, text="Quit", command=master.destroy)
-Quit.place(x=0, y=125)
+    btn2 = Button(master, 
+                text ="Encrypt", 
+                command = enc)
+    btn3 = Button(master, 
+                text ="Decrypt", 
+                command = dec)
 
-mainloop()
+    btn4 = Button(master, 
+                text ="Quit", 
+                command = master.destroy)
+                
+    btn.pack(pady = 7)
+    btn2.pack(pady = 7)
+    btn3.pack(pady = 7)
+    btn4.pack(pady = 7)
+
+    master.mainloop()
+
+def gen():
+    from Crypto.PublicKey import RSA
+    #generate private key
+    key = RSA.generate(2048)
+    private_key = key.export_key()
+    file_out = open("private.pem", "wb")
+    file_out.write(private_key)
+    file_out.close()
+    #generate public key
+    public_key = key.publickey().export_key()
+    file_out = open("public.pem", "wb")
+    file_out.write(public_key)
+    file_out.close()
+
+main()
